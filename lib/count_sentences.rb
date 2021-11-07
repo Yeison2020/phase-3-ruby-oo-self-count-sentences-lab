97f3349dca61880905b7ce7037ecdb99060b90d4
Yeison <enmanuelquiterios@gmail.com>
2021-11-06 23:19:34 -0400

    Completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,34 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    if self[-1] == '.'
+      return true
+    else 
+      return false
+    end
   end
 
   def question?
+    if self[-1] == "?"
+      return true
+    else
+      return false
+    end
 
   end
 
   def exclamation?
+    if self[-1] == "!"
+      return true
+    else
+      return false
+    end
 
   end
 
   def count_sentences
+    word = self.split(/(?<!')\.(?!!.,')/)
+    return word.size
 
-  end
 end
